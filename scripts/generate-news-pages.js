f2778eb985ffd429a7605b30e4df55689ab12b04
SaleemSial1 <121001969+SaleemSial1@users.noreply.github.com>
2026-09-19 15:12:30 +0500

    Polish clean theme color system

--- scripts/generate-news-pages.js
+++ scripts/generate-news-pages.js
@@ -10,7 +10,7 @@ const {
   loadNews,
 } = require('./news-utils');
 
-const stylesHref = '/styles.css?v=20260919-clean';
+const stylesHref = '/styles.css?v=20260919-ux';
 
 function ensureDir(dir) {
   fs.mkdirSync(dir, { recursive: true });
